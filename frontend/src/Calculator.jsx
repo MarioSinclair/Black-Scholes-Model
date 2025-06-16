9ba26cc4748445122f039f124a18d3debb75b547
MarioSinclair <mario.sinclairbz@gmail.com>
2025-06-16 18:33:12 -0400

    New Changes

--- frontend/src/Calculator.jsx
+++ frontend/src/Calculator.jsx
@@ -66,7 +66,7 @@ export default function Calculator() {
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 0.4 }}
-          viewport={{margin: "-200px"}}
+          viewport={{once: true}}
         >
         <div className="main-heading">
           <p>Calculator</p>
