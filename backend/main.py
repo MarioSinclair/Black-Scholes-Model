6fdeea66e894ab47cb2ad3725535ccae85f364bb
MarioSinclair <mario.sinclairbz@gmail.com>
2025-06-16 18:25:23 -0400

    New Changes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from black_scholes import black_scholes_calculation
+from backend.black_scholes import black_scholes_calculation
 
 app = FastAPI()
 
