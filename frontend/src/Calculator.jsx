d49e830b8a22f88971492a2ddb503642b2d29531
MarioSinclair <mario.sinclairbz@gmail.com>
2025-07-14 01:10:24 -0400

    Added Greeks Section

--- frontend/src/Calculator.jsx
+++ frontend/src/Calculator.jsx
@@ -54,7 +54,7 @@ export default function Calculator() {
 
   const fetchPrices = async () => {
     try {
-      const res = await axios.post("http://localhost:8000/api/price", params);
+      const res = await axios.post("/api/price", params);
       setOptionsPrice(res.data);
     } catch (error) {
       console.error("Error fetching prices:", error);
